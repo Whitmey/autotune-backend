d5c38ec4c0b5975d0acd48f202dfd15a023a9e33
WillWhitmey <will.whitmey@gojip2p.com>
2019-01-03 09:15:10 +0000

    added ability to run shell scripts from service

--- api/services/service.js
+++ api/services/service.js
@@ -1,6 +1,8 @@
 'use strict';
+const shell = require('shelljs');
 
 exports.run_algorithm = function(req, res) {
+  shell.exec('oref0-autotune --dir=~/myopenaps --ns-host=https:william-cgm.herokuapp.com --start-date=YYYY-MM-DD');
   res.sendStatus(200);
 }
 
